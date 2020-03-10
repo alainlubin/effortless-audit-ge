4ed7dd830463fff5e241cb12f0ad72b8db6dd876
Alain Lubin <alubin@chef.io>
2020-03-10 14:02:03 -0400

    added azure code

---
+++ controls/windows_audit.rb
@@ -0,0 +1,8 @@
+# encoding: utf-8
+# copyright: 2018, The Authors
+
+title 'Effortless Workshop'
+
+describe file ('c:/helloworld.txt') do
+    it {should exist}
+end
---
+++ habitat/plan.ps1
@@ -0,0 +1,7 @@
+$pkg_name="effortless-audit"
+$pkg_origin="jmassardo"
+$pkg_version="0.2.0"
+$pkg_maintainer="James Massardo <james@dxrf.com>"
+$pkg_license=("Apache-2.0")
+$pkg_description="Effortless Windows Audit"
+$pkg_scaffolding="chef/scaffolding-chef-inspec"
